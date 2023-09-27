fed9b725787ad59bf586cb678b43ecba39b451dc
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-09-28 00:37:31 +0300

    blog

--- next/src/app/(dynamic)/blog/layout.jsx
+++ next/src/app/(dynamic)/blog/layout.jsx
@@ -1,7 +1,7 @@
 export default function BlogLayout({children}) {
   return (
     <div>
-        BlogLayout
+        
         {children}
         </div>
   )
--- next/src/app/(dynamic)/blog/page.jsx
+++ next/src/app/(dynamic)/blog/page.jsx
@@ -1,7 +1,105 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css"
 
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div className={styles.container}>
+<Link href="/blog/id" className={styles.post}>
+<div className={styles.ImageContainer}>
+<Image
+className={styles.image}
+src={"https://images.pexels.com/photos/18113138/pexels-photo-18113138/free-photo-of-horses-grazing-on-meadow.jpeg"}
+width={350}
+height={250}
+alt="post image"
+
+/>
+</div>
+<div className={styles.content}>
+<h1 className={styles.title}>Post Title</h1>
+<p className={styles.text}> Post Text</p>
+
+</div>
+
+
+
+
+</Link >
+
+
+<Link href="/blog/id" className={styles.post}>
+<div className={styles.ImageContainer}>
+<Image
+className={styles.image}
+src={"https://images.pexels.com/photos/18113138/pexels-photo-18113138/free-photo-of-horses-grazing-on-meadow.jpeg"}
+width={350}
+height={250}
+alt="post image"
+
+/>
+</div>
+<div className={styles.content}>
+<h1 className={styles.title}>Post Title</h1>
+<p className={styles.text}> Post Text</p>
+
+</div>
+
+
+
+
+</Link >
+
+
+
+<Link href="/blog/id" className={styles.post}>
+<div className={styles.ImageContainer}>
+<Image
+className={styles.image}
+src={"https://images.pexels.com/photos/18113138/pexels-photo-18113138/free-photo-of-horses-grazing-on-meadow.jpeg"}
+width={350}
+height={250}
+alt="post image"
+
+/>
+</div>
+<div className={styles.content}>
+<h1 className={styles.title}>Post Title</h1>
+<p className={styles.text}> Post Text</p>
+
+</div>
+
+
+
+
+</Link >
+
+
+
+
+<Link href="/blog/id" className={styles.post}>
+<div className={styles.ImageContainer}>
+<Image
+className={styles.image}
+src={"https://images.pexels.com/photos/18113138/pexels-photo-18113138/free-photo-of-horses-grazing-on-meadow.jpeg"}
+width={350}
+height={250}
+alt="post image"
+
+/>
+</div>
+<div className={styles.content}>
+<h1 className={styles.title}>Post Title</h1>
+<p className={styles.text}> Post Text</p>
+
+</div>
+
+
+
+
+</Link >
+
+    </div>
   )
 }
